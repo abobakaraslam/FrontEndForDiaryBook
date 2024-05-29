67b4f3fe35a9a362872c9e3db5647476638ef94c
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2024-05-29 18:31:45 +0500

    Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,19 +6,19 @@ function Navbar() {
     <div>
       {/* Navbar Container */}
       <nav className="bg-black p-4">
-        <a href="/" className="px-3 hover:text-red-500 text-white">
+        <a href="#" className="px-3 hover:text-red-500 text-white">
           Home
         </a>
-        <a href="/diary" className="px-3 hover:text-red-500 text-white">
+        <a href="#" className="px-3 hover:text-red-500 text-white">
           Diary
         </a>
-        <a href="/register-user" className="px-3 hover:text-red-500 text-white">
+        <a href="#" className="px-3 hover:text-red-500 text-white">
           Register
         </a>
-        <a href="/login-user" className="px-3 hover:text-red-500 text-white">
+        <a href="#" className="px-3 hover:text-red-500 text-white">
           Login
         </a>
-        <a href="/logout" className="px-3 hover:text-red-500 text-white">
+        <a href="#" className="px-3 hover:text-red-500 text-white">
           Logout
         </a>
       </nav>
